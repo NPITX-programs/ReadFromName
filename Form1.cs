d8d366f3a1accbc4668a2945b34c4e7f6b676560
CTH-npitx <chatch@npitx.org>
2025-03-31 14:09:35 -0500

    comment

--- Form1.cs
+++ Form1.cs
@@ -28,8 +28,10 @@ namespace ReadFromName
               msg = msg + ": " + ex.Message;
             }
             MessageBox.Show(msg);
-        } 
-        
+        } //this is for error messages. the text is mandatory, but the
+          //rest isn't necessary. if you don't provide an exception, it won't display one. If you provide an error, it will default to "hide error".
+          //If you provide a boolean, you can controll if it's hidden. If you provide a boolean but no error, well, it won't work right
+
         private string fullPath()
         {
             string fullPath = folderName + fileName + fileExtension;
