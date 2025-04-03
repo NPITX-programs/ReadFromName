732be8b57a081d4bd1c497cf53657d70050e6f2f
CTH-npitx <chatch@npitx.org>
2025-04-03 11:56:41 -0500

    added the button and list box for the third way of doing it

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,8 +35,12 @@
             this.btn_triggerAll = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btn_stringReader = new System.Windows.Forms.Button();
+            this.lst_stringReader = new System.Windows.Forms.ListBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // lst_readAllText
@@ -92,7 +96,7 @@
             this.groupBox1.Controls.Add(this.lst_readAllText);
             this.groupBox1.Location = new System.Drawing.Point(345, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(452, 157);
+            this.groupBox1.Size = new System.Drawing.Size(452, 114);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
@@ -108,11 +112,41 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btn_stringReader);
+            this.groupBox3.Controls.Add(this.lst_stringReader);
+            this.groupBox3.Location = new System.Drawing.Point(345, 332);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(452, 116);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // btn_stringReader
+            // 
+            this.btn_stringReader.Location = new System.Drawing.Point(0, 0);
+            this.btn_stringReader.Name = "btn_stringReader";
+            this.btn_stringReader.Size = new System.Drawing.Size(75, 44);
+            this.btn_stringReader.TabIndex = 1;
+            this.btn_stringReader.Text = "String Reader";
+            this.btn_stringReader.UseVisualStyleBackColor = true;
+            // 
+            // lst_stringReader
+            // 
+            this.lst_stringReader.FormattingEnabled = true;
+            this.lst_stringReader.HorizontalScrollbar = true;
+            this.lst_stringReader.Location = new System.Drawing.Point(81, 0);
+            this.lst_stringReader.Name = "lst_stringReader";
+            this.lst_stringReader.Size = new System.Drawing.Size(370, 108);
+            this.lst_stringReader.TabIndex = 0;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btn_triggerAll);
@@ -120,6 +154,7 @@
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -133,6 +168,9 @@
         private System.Windows.Forms.Button btn_triggerAll;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btn_stringReader;
+        private System.Windows.Forms.ListBox lst_stringReader;
     }
 }
 
